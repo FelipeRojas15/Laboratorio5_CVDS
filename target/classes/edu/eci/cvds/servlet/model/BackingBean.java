3c92b9d469cebcb7012005b408fcf96ab097c992
Kyrie <james.weisnner@mail.escuelaing.edu.co>
2019-09-27 19:37:29 -0500

    parte II finalizada, laboratorio finalizado

--- target/classes/edu/eci/cvds/servlet/model/BackingBean.java
+++ target/classes/edu/eci/cvds/servlet/model/BackingBean.java
@@ -7,9 +7,9 @@ package edu.eci.cvds.servlet.model;
 
 
 import static java.lang.Math.random;
+import java.util.ArrayList;
 import java.util.Random;
-import javax.enterprise.context.ApplicationScoped;
-import javax.faces.bean.ManagedBean;
+import javax.faces.bean.*;
 
 
 
@@ -24,6 +24,10 @@ import javax.faces.bean.ManagedBean;
  *
  * El estado del juego, que sería una cadena de texto que indica si ya ganó o
  * no, y si ganó de cuanto es el premio.
+ * 
+ * El juego consiste en adivinar un numero al azar de 1 a 10, cada intento resta 10.000
+ * al premio final, el premio comienza siendo 100.000. Al llegar a un premio de 0 o menos
+ * el juego se pierde y tendra que ser reiniciado
 
  */
 @ManagedBean(name = "guessBean")
@@ -34,45 +38,74 @@ public class BackingBean {
     private int attempts;
     private int price;
     private String state;
-    private int inputNumber;
-
-
+    private int inputNumber;    
+    private ArrayList<Integer> attemptedNumbers;
     
-    
-    
-    public BackingBean(){      
+    private final String winState="YOU WIN!!!"; 
+    private final String loseState="YOU LOSE!!!";
+    private final String initialState="Try to guess the number!";
+    private final String wrongState="That's the wrong number";
+    private final String invalidState="You are using an invalid number";
+       
+        
+    public BackingBean(){ 
         Random randNumber = new Random();
-        guessNumber = 1+randNumber.nextInt(10);
+        guessNumber = 1+randNumber.nextInt(15);
         attempts = 0;
         price=100000;
-        state="Keep Guessing";        
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        state=initialState;        
+        attemptedNumbers = new ArrayList<Integer>();
     }
+
     
-    public void guess(){
-        attempts += 1;
-        if (inputNumber == guessNumber){
-            state="YOU WIN!";    
+    public void guess(int inputNumber){
+        
+        if (!state.equals(winState)){
+            if (inputNumber==guessNumber & price>0){
+                attempts +=1;                                
+                setState(winState);
+            }
+            else if (inputNumber!=guessNumber & price>0){
+                attempts +=1;                
+                attemptedNumbers.add(inputNumber);
+                price -=10000;
+                setState(wrongState);
+            }
+            else if (price<=0){
+                setState(loseState);
+            }
+        }else{
+            setState(winState);
             
-        }                
-        else{
-            price-=10000;
-        }        
-        if((price-10000)<0){            
-            state="YOU LOSE!";
         }
 
-        
-        
-        
+       
     }
-    
+    public void guess(String inputNumber){
+        try{
+            
+            this.inputNumber = Integer.parseInt(inputNumber);  
+            guess(this.inputNumber);
+        
+        }catch(Exception e){
+            setState(invalidState);
+        }
+    }
     public void restart (){
         Random randNumber = new Random();
         guessNumber = 1+randNumber.nextInt(10);
         attempts = 0;
         price=100000;
-        state="Keep Guessing";  
+        state=initialState; 
+        attemptedNumbers.clear();
+    }
+
+    public ArrayList<Integer> getAttemptedNumbers() {
+        return attemptedNumbers;
+    }
+
+    public void setAttemptedNumbers(ArrayList<Integer> attemptedNumbers) {
+        this.attemptedNumbers = attemptedNumbers;
     }
 
     public int getInputNumber() {
